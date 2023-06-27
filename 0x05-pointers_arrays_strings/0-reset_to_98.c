3ad288ab9768fe89e435c018cc8f2d3bab0176de
nada <nadaayoub362@gmail.com>
2023-06-27 06:43:02 -0700

    m

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - take a pointer to an int paramter
+ *               and update its value to point to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
