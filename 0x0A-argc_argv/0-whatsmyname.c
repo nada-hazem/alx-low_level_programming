1cf5d6e8b39195494ce929e5a4add9311258d3c9
nada <nadaayoub362@gmail.com>
2023-07-10 19:58:28 -0700

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * main - print program name, followed by new line
+ * main - prints program name, followed by new line
  * @argc: int
  * @argv: list
  * Return: 0
-*/
+ */
 
 int main(int argc, char const *argv[])
 {
