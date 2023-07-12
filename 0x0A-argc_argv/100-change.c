976bae275855a212d2b7583ff37d71f337b0eb96
nada <nadaayoub362@gmail.com>
2023-07-12 05:23:42 -0700

    m

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdib.h>
+
+/**
+ * main-prints the minimum number of coins
+ * to make change gor ana amount of money
+ * @argc:number of comandline arguments
+ * @argv:pointer to an array of command line argumets
+ * Return:0-success, non-zero-fail
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, leastcents = 0, money = atoi(argv[i]);
+		int cents[] = {25, 10, 5, 2, 1};
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				leastcents += money / centa[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+
+			}
+		}
+		printf("%d\n", leastcents);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
