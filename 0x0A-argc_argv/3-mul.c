e9a09daf05e8612b637eb05645a0fa20baf9a6b7
nada <nadaayoub362@gmail.com>
2023-07-12 06:05:43 -0700

    m

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * main-print sum of 2 numbers
- * @argc:number of commandline arguments
- * @argv:pointer to an array of command line arguments
- * Return:0-success, non-zero-fail
+ *main-print sum of 2 numbers
+ *@argc:number of commandline arguments
+ *@argv:pointer to an array of command line arguments
+ *Return:0-success, non-zero-fail
  */
 
 int main(int argc, char *argv[])
