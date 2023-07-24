de132230717794f24239e5c9a0c2f32085119de6
Noran Sherif <>
2023-07-24 14:50:38 -0700

    m

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+/**
+ * main -print number of arguments passed
+ * @argc:nmuber of commandline arguments
+ * @argv:pointer to an array of command line arguments
+ * Return:0-sucess, non-zero-fail
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
